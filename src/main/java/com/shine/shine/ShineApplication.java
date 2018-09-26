26950975887e2bd3169f80d785b24aef0726db2d
Javad Alimohammadi <bs.alimohammadi@gmail.com>
2018-09-26 09:21:21 +0330

    first commit

---
+++ src/main/java/com/shine/shine/ShineApplication.java
@@ -0,0 +1,12 @@
+package com.shine.shine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShineApplication.class, args);
+	}
+}
