97133b21a2e64f393ca263d9e2e93c9ba8bfad8b
Javad Alimohammadi <bs.alimohammadi@gmail.com>
2018-11-02 20:02:48 +0330

    add voting capability

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
 import './App.css'
+import { Switch } from 'react-router-dom'
+import HOME from './components/HOME'
+import ERROR from './components/ERROR'
+import UnAnswered from './components/UnAnswered'
 import Layout from './components/Layout/Layout'
 import Question from './containers/Question/Question'
+import ASK from './containers/Ask/ASK'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      pageTitle: ''
+    }
+
+    // Event handler
+
+  }
+
   render () {
+
     return (
       <div>
-        <Layout>
-          <Question/>
-        </Layout>
-        {/*<Switch>*/}
-        {/*<Route path="/" exact component={HOME}/>*/}
-        {/*<Route path="/question" component={Question}/>*/}
-        {/*<Route path="/unanswered" component={UnAnswered}/>*/}
-        {/*<Route component={ERROR}/>*/}
-        {/*</Switch>*/}
+        <Switch>
+          <Layout path="/" exact component={HOME} titleOfPage='Home'/>
+          <Layout path="/question" component={Question} titleOfPage='Recent questions'/>
+          <Layout path="/ask" component={ASK} titleOfPage='Ask a question'/>
+          <Layout path="/un-answered" component={UnAnswered} titleOfPage='Recent questions without answers'/>
+          <Layout component={ERROR} titleOfPage='Error in reposnse of page'/>
+        </Switch>
+
       </div>
     )
   }
--- Client/src/components/Layout/Layout.js
+++ Client/src/components/Layout/Layout.js
@@ -1,41 +1,45 @@
 import React from 'react'
-import AUX from '../../hoc/_Aux'
 import Navigation from '../../containers/Navigation/Navigation'
 import Footer from '../../containers/Footer/Footer'
+import { Route } from 'react-router-dom'
 import Search from '../../containers/Search/Search'
+import AUX from '../../hoc/_Aux'
 
-const layout = (props) => (
-  <AUX>
-    <Navigation/>
+const layout = ({component: Component, ...rest}) => {
+  return (
 
-    <main className="donut-masthead">
-      <div className="container">
-        <div className="page-title">
-          <a href="./index.php?qa=feed&amp;qa_1=questions.rss" title="" className="qa-rss-feed"
-             data-original-title="Recent questions"><i className="fa fa-rss qa-rss-icon"></i></a>
-          <h1>
-            Recent questions
-          </h1>
-        </div>
-      </div>
-    </main>
+    <Route {...rest} render={matchProps => (
+      <AUX>
+        <Navigation/>
 
-    <div className="qa-body-wrapper container">
-      <div className="qa-main-shadow clearfix">
-        <div className="qa-main-wrapper clearfix row">
-          <div className="qa-main col-md-9 col-xs-12 pull-left">
-            {props.children}
+        <main className="donut-masthead">
+          <div className="container">
+            <div className="page-title">
+              <h1>
+                {rest.titleOfPage}
+              </h1>
+            </div>
           </div>
-          <div className="qa-sidepanel col-md-3 col-xs-12 pull-right">
-            <Search/>
+        </main>
+
+        <div className="qa-body-wrapper container">
+          <div className="qa-main-shadow clearfix">
+            <div className="qa-main-wrapper clearfix row">
+              <div className="qa-main col-md-9 col-xs-12 pull-left">
+                <Component {...matchProps}/>
+              </div>
+              <div className="qa-sidepanel col-md-3 col-xs-12 pull-right">
+                <Search/>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
 
-
-    <Footer/>
-  </AUX>
-)
+        <Footer/>
+      </AUX>
+    )}
+    />
+  )
+}
 
 export default layout
