84b0433791b2c95930e759deb0524e4de022c341
Javad Alimohammadi <bs.alimohammadi@gmail.com>
2019-01-18 18:50:10 +0330

    add login page

--- Client/src/containers/User/Login/Login.js
+++ Client/src/containers/User/Login/Login.js
@@ -7,55 +7,47 @@ class Login extends Component {
       <div className="qa-part-form">
         <h1 className="text-center">Login</h1>
         <div className="col-md-5">
-        <form method="post" className="col-mod-10">
-          <table className="qa-form-tall-table">
-            <tbody>
-            <tr>
-              <td className="qa-form-tall-label">
-                Username:
-              </td>
-            </tr>
-            <tr>
-              <td className="qa-form-tall-data">
-                <input name="handle" id="handle" dir="auto" type="text" 
-                       className="qa-form-tall-text form-control"/>
-              </td>
-            </tr>
-            <tr>
-              <td className="qa-form-tall-label">
-                Password:
-              </td>
-            </tr>
-            <tr>
-              <td className="qa-form-tall-data">
-                <input name="password" id="password" dir="auto" type="password"
-                       className="qa-form-tall-text form-control"/>
-              </td>
-            </tr>
-            <tr>
-              <td className="qa-form-tall-label">
-                Email:
-              </td>
-            </tr>
-            <tr>
-              <td className="qa-form-tall-data">
-                <input name="email" id="email" dir="auto" type="text"
-                       className="qa-form-tall-text form-control"/>
-                <div className="qa-form-tall-note">Privacy: Your email address will not be shared or sold to third
-                  parties.
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="col-md-4 text-center">
-                <button type="submit"
-                        className="qa-form-tall-button qa-form-tall-button-register ">Register
-                </button>
-              </td>
-            </tr>
-            </tbody>
-          </table>
-        </form>
+          <form method="post" className="col-mod-10">
+            <table className="qa-form-tall-table">
+              <tbody>
+              <tr>
+                <td className="qa-form-tall-label">
+                  Username:
+                </td>
+              </tr>
+              <tr>
+                <td className="qa-form-tall-data">
+                  <input name="handle" id="handle" dir="auto" type="text"
+                         className="qa-form-tall-text form-control"/>
+                </td>
+              </tr>
+              <tr>
+                <td className="qa-form-tall-label">
+                  Password:
+                </td>
+              </tr>
+              <tr>
+                <td className="qa-form-tall-data">
+                  <input name="password" id="password" dir="auto" type="password"
+                         className="qa-form-tall-text form-control"/>
+                </td>
+              </tr>
+              <tr>
+                <td className="col-md-4 text-center">
+                  <button type="submit"
+                          className="qa-form-tall-button qa-form-tall-button-login ">Login
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td className="col-md-4 text-center">
+                  <a href={'/'}>Register
+                  </a>
+                </td>
+              </tr>
+              </tbody>
+            </table>
+          </form>
         </div>
         <div className="vl col-md-1">
         </div>
