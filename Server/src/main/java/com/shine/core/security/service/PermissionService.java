567189e8445ae5af4e8a9a32d003122327a9cbd7
Javad Alimohammadi <bs.alimohammadi@gmail.com>
2019-03-31 01:01:24 +0430

    removed unused type

--- Server/src/main/java/com/shine/core/security/service/PermissionService.java
+++ Server/src/main/java/com/shine/core/security/service/PermissionService.java
@@ -1,7 +1,6 @@
 package com.shine.core.security.service;
 
 import com.shine.core.security.PermissionType;
-import com.shine.core.security.PermissionValueType;
 import com.shine.core.security.domain.Permission;
 import com.shine.core.security.domain.ShineRole;
 
@@ -16,13 +15,11 @@ public interface PermissionService {
      *
      * @param permissionType
      * @param permissionTarget
-     * @param permissionValueType
      * @param permissionValue
      * @param shineRole
      * @return New persistent permission
      */
 
     Permission createPermission(PermissionType permissionType, String permissionTarget,
-                                PermissionValueType permissionValueType,
                                 Integer permissionValue, ShineRole shineRole);
 }
