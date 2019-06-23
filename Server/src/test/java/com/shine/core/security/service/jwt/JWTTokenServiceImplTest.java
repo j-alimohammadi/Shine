2af2512299abb3550487e0e55311d76c77ef0be2
javad alimohammadi <bs.alimohammadi@gmail.com>
2019-06-23 10:30:57 +0430

    adding test

---
+++ Server/src/test/java/com/shine/core/security/service/jwt/JWTTokenServiceImplTest.java
@@ -0,0 +1,27 @@
+package com.shine.core.security.service.jwt;
+
+import org.junit.Test;
+
+/**
+ * @author Javad Alimohammadi<bs.alimohammadi@gmail.com>
+ */
+
+public class JWTTokenServiceImplTest {
+
+    private JWTTokenService jwtTokenService = new JWTTokenServiceImpl();
+
+    @Test
+    public void generateAuthenticationToken() {
+        String userName = "john";
+        String sessionId = "123";
+
+        JWTInfo jwtInfo = jwtTokenService.generateAuthenticationToken(userName, sessionId);
+
+
+        
+    }
+
+    @Test
+    public void parseToken() {
+    }
+}
